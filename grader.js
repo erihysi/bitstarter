d3248890397cd2330544489cfc8f6c0a49c37b0c
Ubuntu <ubuntu@ip-172-31-39-75.us-west-2.compute.internal>
2013-07-10 16:50:49 +0000

    Submited grader files

--- grader.js
+++ grader.js
@@ -24,14 +24,17 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var sys = require('util');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
+var TEMP_FILE = "temp.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
-        console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+	console.log("%s does not exist. Exiting.", instr);
+	process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
 };
@@ -49,8 +52,8 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
-        out[checks[ii]] = present;
+	var present = $(checks[ii]).length > 0;
+	out[checks[ii]] = present;
     }
     return out;
 };
@@ -61,14 +64,35 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var getUrl = function(url,checksFile){
+  rest.get(url).on('complete', function(result) {
+      if (result instanceof Error) {
+	sys.puts('Error: ' + result.message);
+      } else {
+	//sys.puts(result);
+	fs.writeFileSync(TEMP_FILE,result);
+	var checkJson = checkHtmlFile(TEMP_FILE, checksFile);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+      }
+    });
+};
+
 if(require.main == module) {
     program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
-} else {
+	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists))
+	.option('-u, --url <url_file>','URL Path to file')
+	.parse(process.argv);
+    if (program.url) getUrl(program.url,program.checks);
+    if (program.file){
+	console.log('Entered file');
+	var checkJson = checkHtmlFile(program.file, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+	fs.writeFileSync('result.json',outJson);
+    }
+}
+else {
     exports.checkHtmlFile = checkHtmlFile;
 }
